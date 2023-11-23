f6b0fce93fa24d439b45ddda1e607004f24b1221
ardhiesta <ardhi.mail@gmail.com>
2023-11-23 22:07:49 +0800

    first commit

---
+++ src/com/latihan/Main.java
@@ -0,0 +1,9 @@
+package com.latihan;
+
+public class Main {
+    public static void main(String[] args) {
+        Karyawan karyawan1 = new Karyawan();
+        karyawan1.beriNama("Supardi Charleson");
+        System.err.println("nama : "+karyawan1.nama);
+    }
+}
